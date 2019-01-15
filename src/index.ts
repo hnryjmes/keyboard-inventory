1390f75ec7e4302bd2c3c9186f467b58707cd8c7
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-15 10:49:00 +0000

    Add view engine to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import path from "path";
 const app = express();
 const port = 8080; // default port to listen
 
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
 // define a route handler for the default home page
 app.get("/", (req, res) => {
   // render the index template
