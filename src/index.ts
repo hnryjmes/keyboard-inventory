8c3541ca3fe743af3c82475e0454edd271c4720e
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-15 10:42:16 +0000

    Update index.ts to render index template

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  // render the index template
+  res.render("index");
 });
 
 // start the Express server
