1e236fb4ff07df4bc0fe4c1d0ff064e68d53ecd9
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-15 14:35:39 +0000

    Add lots of @ts-ignore to main.ts

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import * as M from "materialize-css";
 import Vue from "vue";
-
 // tslint:disable no-unused-expression
 new Vue({
   // eslint-disable-line no-new
   computed: {
     hazKeyboards(): boolean {
+      // @ts-ignore
       return this.isLoading === false && this.keyboards.length > 0;
     },
     noKeyboards(): boolean {
+      // @ts-ignore
       return this.isLoading === false && this.keyboards.length === 0;
     }
   },
@@ -29,19 +30,29 @@ new Vue({
   methods: {
     addKeyboard() {
       const keyboard = {
+        // @ts-ignore
         brand: this.brand,
+        // @ts-ignore
         color: this.color,
+        // @ts-ignore
         model: this.model,
+        // @ts-ignore
         year: this.year
       };
       axios
         .post("/api/keyboards/add", keyboard)
         .then(() => {
+          // @ts-ignore
           this.$refs.year.focus();
+          // @ts-ignore
           this.brand = "";
+          // @ts-ignore
           this.color = "";
+          // @ts-ignore
           this.model = "";
+          // @ts-ignore
           this.year = "";
+          // @ts-ignore
           this.loadKeyboards();
         })
         .catch((err: any) => {
@@ -50,10 +61,14 @@ new Vue({
         });
     },
     confirmDeleteKeyboard(id: string) {
+      // @ts-ignore
       const keyboard = this.keyboards.find((k: any) => k.id === id);
+      // @ts-ignore
       this.selectedKeyboard = `${keyboard.year} ${keyboard.brand} ${keyboard.model}`;
+      // @ts-ignore
       this.selectedKeyboardId = keyboard.id;
       const dc = this.$refs.deleteConfirm;
+      // @ts-ignore
       const modal = M.Modal.init(dc);
       // const modal = M.Modal.getInstance( dc );
       modal.open();
@@ -61,6 +76,7 @@ new Vue({
     deleteKeyboard(id: string) {
       axios
         .delete(`/api/keyboards/remove/${id}`)
+        // @ts-ignore
         .then(this.loadKeyboards)
         .catch((err: any) => {
           // tslint:disable no-console
@@ -71,7 +87,9 @@ new Vue({
       axios
         .get("/api/keyboards/all")
         .then((res: any) => {
+          // @ts-ignore
           this.isLoading = false;
+          // @ts-ignore
           this.keyboards = res.data;
         })
         .catch((err: any) => {
@@ -81,6 +99,7 @@ new Vue({
     }
   },
   mounted() {
+    // @ts-ignore
     return this.loadKeyboards();
   }
 });
