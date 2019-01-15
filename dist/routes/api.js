6e4d0dae0b5da161995184be2977247f7cb945d8
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-15 12:34:48 +0000

    Add get routes to api.ts

---
+++ dist/routes/api.js
@@ -0,0 +1,88 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_promise_1 = __importDefault(require("pg-promise"));
+exports.register = (app) => {
+    const oidc = app.locals.oidc;
+    const port = parseInt(process.env.PGPORT || "5432", 10);
+    const config = {
+        database: process.env.PGDATABASE || "postgres",
+        host: process.env.PGHOST || "localhost",
+        port,
+        user: process.env.PGUSER || "postgres"
+    };
+    const pgp = pg_promise_1.default();
+    const db = pgp(config);
+    app.get(`/api/keyboards/all`, oidc.ensureAuthenticated(), (req, res) => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const userId = req.userContext.userinfo.sub;
+            const keyboards = yield db.any(`
+        SELECT
+            id
+            , brand
+            , model
+            , year
+            , color
+        FROM    keyboards
+        WHERE   user_id = $[userId]
+        ORDER BY year, brand, model`, { userId });
+            return res.json(keyboards);
+        }
+        catch (err) {
+            // tslint:disable-next-line: no-console
+            console.error(err);
+            res.json({ error: err.message || err });
+        }
+    }));
+    app.get(`/api/keyboards/total`, oidc.ensureAuthenticated(), (req, res) => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const userId = req.userContext.userinfo.sub;
+            const total = yield db.one(`
+      SELECT  count(*) AS total
+      FROM    keyboards
+      WHERE   user_id = $[userId]`, { userId }, (data) => {
+                return {
+                    total: +data.total
+                };
+            });
+            return res.json(total);
+        }
+        catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+            res.json({ error: err.message || err });
+        }
+    }));
+    app.get(`/api/keyboards/find/:search`, oidc.ensureAuthenticated(), (req, res) => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const userId = req.userContext.userinfo.sub;
+            const keyboards = yield db.any(`
+        SELECT
+            id
+            , brand
+            , model
+            , year
+            , color
+        FROM    keyboards
+        WHERE   user_id = $[userId]
+        AND    (brand ILIKE $[search] OR model ILIKE $[search])`, { userId, search: `%${req.params.search}%` });
+            return res.json(keyboards);
+        }
+        catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+            res.json({ error: err.message });
+        }
+    }));
+};
+//# sourceMappingURL=api.js.map
---
+++ src/routes/api.ts
@@ -0,0 +1,78 @@
+import * as express from "express";
+import pgPromise from "pg-promise";
+
+export const register = (app: express.Application) => {
+  const oidc = app.locals.oidc;
+  const port = parseInt(process.env.PGPORT || "5432", 10);
+  const config = {
+    database: process.env.PGDATABASE || "postgres",
+    host: process.env.PGHOST || "localhost",
+    port,
+    user: process.env.PGUSER || "postgres"
+  };
+
+  const pgp = pgPromise();
+  const db = pgp(config);
+
+  app.get(`/api/keyboards/all`, oidc.ensureAuthenticated(), async (req: any, res) => {
+    try {
+      const userId = req.userContext.userinfo.sub;
+      const keyboards = await db.any(`
+        SELECT
+            id
+            , brand
+            , model
+            , year
+            , color
+        FROM    keyboards
+        WHERE   user_id = $[userId]
+        ORDER BY year, brand, model`, { userId });
+      return res.json(keyboards);
+    } catch (err) {
+      // tslint:disable-next-line: no-console
+      console.error(err);
+      res.json({ error: err.message || err });
+    }
+  });
+
+  app.get(`/api/keyboards/total`, oidc.ensureAuthenticated(), async (req: any, res) => {
+    try {
+      const userId = req.userContext.userinfo.sub;
+      const total = await db.one(`
+      SELECT  count(*) AS total
+      FROM    keyboards
+      WHERE   user_id = $[userId]`, { userId }, (data: { total: number }) => {
+        return {
+          total: +data.total
+        };
+      });
+      return res.json(total);
+    } catch ( err ) {
+      // tslint:disable-next-line:no-console
+      console.error(err);
+      res.json({ error: err.message || err });
+    }
+  });
+
+  app.get(`/api/keyboards/find/:search`, oidc.ensureAuthenticated(), async (req: any, res) => {
+    try {
+      const userId = req.userContext.userinfo.sub;
+      const keyboards = await db.any(`
+        SELECT
+            id
+            , brand
+            , model
+            , year
+            , color
+        FROM    keyboards
+        WHERE   user_id = $[userId]
+        AND    (brand ILIKE $[search] OR model ILIKE $[search])`,
+        { userId, search: `%${ req.params.search }%` });
+      return res.json(keyboards);
+    } catch (err) {
+      // tslint:disable-next-line:no-console
+      console.error(err);
+      res.json({ error: err.message });
+    }
+  });
+};
