9bd8db13563116d1a6e698bf0eeb340a0e716d9a
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-15 11:44:11 +0000

    Run build

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,1 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.register = (app) => {
+    const oidc = app.locals.oidc;
+    // define a route handler for the default home page
+    app.get("/", (req, res) => {
+        res.render("index");
+    });
+    // define a secure route handler for the login page that redirects to /keyboards
+    app.get("/login", oidc.ensureAuthenticated(), (req, res) => {
+        res.redirect("/keyboards");
+    });
+    // define a route to handle logout
+    app.get("/logout", (req, res) => {
+        req.logout();
+        res.redirect("/");
+    });
+    // define a secure route handler for the keyboards page
+    app.get("/keyboards", oidc.ensureAuthenticated(), (req, res) => {
+        res.render("keyboards");
+    });
+};
 //# sourceMappingURL=index.js.map
